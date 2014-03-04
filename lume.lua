f1449275425c1e80189d954cc628e3987ae96d38
rxi <rxi@users.noreply.github.com>
2014-03-04 22:53:31 +0000

    Added optional arg `squared` to lume.distance()

--- lume.lua
+++ lume.lua
@@ -42,8 +42,9 @@ function lume.pingpong(x)
 end
 
 
-function lume.distance(x1, y1, x2, y2)
-  return math.sqrt((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
+function lume.distance(x1, y1, x2, y2, squared)
+  local s = (x1 - x2) ^ 2 + (y1 - y2) ^ 2
+  return squared and s or math.sqrt(s)
 end
 
 
