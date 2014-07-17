f6174a4b6800bb3a43f4c11ad0166bfc89664f38
rxi <rxi@users.noreply.github.com>
2014-07-17 22:02:39 +0100

    Version 1.4.1

--- lume.lua
+++ lume.lua
@@ -7,7 +7,7 @@
 -- under the terms of the MIT license. See LICENSE for details.
 --
 
-local lume = { _version = "1.4.0" }
+local lume = { _version = "1.4.1" }
 
 local pairs, ipairs = pairs, ipairs
 local type, assert, unpack = type, assert, unpack or table.unpack
