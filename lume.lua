886bd43bcec547d32bd3c5ca6662864f3394fb0b
rxi <rxi@users.noreply.github.com>
2015-02-24 20:42:44 +0000

    Changed getiter() to disallow `nil` values

--- lume.lua
+++ lume.lua
@@ -51,13 +51,12 @@ local iternil = function()
 end
 
 local getiter = function(x)
-  if x == nil then
-    return iternil
-  elseif isarray(x) then
+  if isarray(x) then
     return ipairs
-  else
+  elseif type(x) == "table" then
     return pairs
   end
+  error("expected table", 3)
 end
 
 local iteratee = function(x)
@@ -327,9 +326,11 @@ function lume.concat(...)
   local rtn = {}
   for i = 1, select("#", ...) do
     local t = select(i, ...)
-    local iter = getiter(t)
-    for k, v in iter(t) do
-      rtn[#rtn + 1] = v
+    if t ~= nil then
+      local iter = getiter(t)
+      for k, v in iter(t) do
+        rtn[#rtn + 1] = v
+      end
     end
   end
   return rtn
