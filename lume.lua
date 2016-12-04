da0d1bbae7be15f7dd4c26068b3df16a707f47fb
rxi <rxi@users.noreply.github.com>
2016-12-04 14:13:10 +0000

    Updated copyright year: 2015 -> 2016

--- lume.lua
+++ lume.lua
@@ -1,7 +1,7 @@
 --
 -- lume
 --
--- Copyright (c) 2015 rxi
+-- Copyright (c) 2016 rxi
 --
 -- This library is free software; you can redistribute it and/or modify it
 -- under the terms of the MIT license. See LICENSE for details.
